18b1475d506d32b9525d30bf894174648c0fdff6
Mike Elsmore <mike@elsmore.me>
2014-08-03 00:09:59 +0100

    Procfile...

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/monsters', monsters);
+app.use('/monster', monsters);
 app.use('/loot', loot);
 app.use('/fight', fight);
 
