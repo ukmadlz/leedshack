f8d957e9f889f4e5a35f8b3c7396887579a3cb65
Mike Elsmore <mike@elsmore.me>
2014-08-02 16:46:46 +0100

    Add names from wikipedia to randomise names

--- models/monster.js
+++ models/monster.js
@@ -2,12 +2,129 @@
 'use strict';
 
 var monster = {
+  // Monsters from wikipedia...plus extras
+  monsters: [
+    "@cbetta",
+    "@jna_sh",
+    "Frankenstein",
+    "Giant",
+    "English Giant",
+    "Greek Giant",
+    "Irish Giant",
+    "Jötnar",
+    "King Kong",
+    "Kaiju",
+    "Gamera",
+    "Godzilla",
+    "King Kong",
+    "Mothra",
+    "Toho",
+    "Ogres",
+    "Slender Man",
+    "Vampires",
+    "Werewolves",
+    "Akkorokamui",
+    "Alraune",
+    "Anguirus",
+    "Argus Panoptes",
+    "Aspidochelone",
+    "Ayia Napa sea monster",
+    "Balaur",
+    "Baragon",
+    "Beast of Gévaudan",
+    "Behemoth",
+    "Bishop-fish",
+    "Bride of Frankenstein",
+    "Cerberus",
+    "Ceto",
+    "Cetus",
+    "Charybdis",
+    "Chimera",
+    "Chinese dragon",
+    "Clover",
+    "Cockatrice",
+    "Coco",
+    "Cookie Monster",
+    "Cretan Bull",
+    "Cthulhu",
+    "Cyclops",
+    "Ebirah",
+    "Echidna",
+    "Elmo",
+    "Erymanthian Boar",
+    "European dragon",
+    "Feilong",
+    "Frankenstein's monster",
+    "Gamera",
+    "Gill-man",
+    "Gog (Marvel Comics)",
+    "Gorgon",
+    "Grendel",
+    "Grendel's mother",
+    "Grover",
+    "Gruagach (Hellboy)",
+    "Guivre",
+    "Hafgufa",
+    "Lernaean Hydra",
+    "Hydros",
+    "Jersey Devil",
+    "Jiangshi",
+    "Jörmungandr",
+    "Kabandha",
+    "Kraken",
+    "Ladon",
+    "Leviathan",
+    "Lyngbakr",
+    "Mares of Diomedes",
+    "Medusa",
+    "Milton the Monster",
+    "Minotaur",
+    "Monster High",
+    "MUTO",
+    "Necrid",
+    "Nemean lion",
+    "Ogdru Hem",
+    "Ogdru Jahad",
+    "Ogre",
+    "Orthrus",
+    "Parallax",
+    "Pixiu",
+    "Psoglav",
+    "Rodan",
+    "Scylla",
+    "Sea monster",
+    "Sea-lion",
+    "Simurgh",
+    "Skeleton",
+    "Sluagh",
+    "Sphinx",
+    "Stronsay Beast",
+    "Swamp monster",
+    "Sweetums",
+    "Telchines",
+    "The Terrible Dogfish",
+    "Tickle monster",
+    "Titan",
+    "Titanosaurus",
+    "Two-Headed Monster",
+    "Wani",
+    "Werehyena",
+    "Werewolf",
+    "Werewoman",
+    "Yeti",
+    "Zombie",
+    "Anzû"
+  ],
+
   /**
+   * Gets a monster
+   * @param {[type]} id [description]
    */
   getMonster: function(id) {
     if(typeof id == 'undefined') {
       return this.getMonsterRandom();
     }
+
     // @TODO modifiers are listed purely to change base monster state against others
 
     return {
@@ -30,11 +147,11 @@ var monster = {
   },
 
   /**
-   * [getMonsterRandomName description]
+   * Gets a random monster name from list crawled off wikipedia
    * @return string
    */
   getMonsterRandomName: function() {
-    return 'Random Name';
+    return this.monsters[Math.floor(Math.random() * this.monsters.length) + 1];
   },
 
   /**
@@ -43,7 +160,7 @@ var monster = {
    * @return integer
    */
   getMonsterRandomLevel: function(base) {
-    return 3;
+    return Math.floor(Math.random() * 10) + 1;
   },
   /**
    * Creates a random monsters modifiers
